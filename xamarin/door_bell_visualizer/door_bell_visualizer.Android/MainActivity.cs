543f9b1d4ed3268e2febc0f4ec889b48c7abaa9a
Max Reble <maxreble@googlemail.com>
2018-08-15 16:47:59 +0200

    added xamarin app

---
+++ xamarin/door_bell_visualizer/door_bell_visualizer.Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+
+namespace door_bell_visualizer.Droid
+{
+    [Activity(Label = "door_bell_visualizer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+
+
+            LoadApplication(new App());
+
+
+
+        }
+
+        protected override void OnStart()
+        {
+            using (Ringtone r = RingtoneManager.GetRingtone(ApplicationContext, RingtoneManager.GetDefaultUri(RingtoneType.Ringtone)))
+            {
+                r.Play();
+            }
+            base.OnStart();
+        }
+    }
+
+
+
+}
+
