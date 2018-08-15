63d3df1145bc29e6d1c151f44bd00c593b0d2dfb
Max Reble <maxreble@googlemail.com>
2018-08-15 21:38:06 +0200

    working with led

--- platformIO/src/main.cpp
+++ platformIO/src/main.cpp
@@ -4,6 +4,8 @@
 #define LED 13
 #define DOOR_BELL 23
 
+
+bool IsRingin();
 void setup() {
     pinMode(LED,OUTPUT);
     pinMode(DOOR_BELL,INPUT);
@@ -15,7 +17,23 @@ void setup() {
 void loop() {
    // int read = 
     //Serial.println(read);
-    digitalWrite(LED,digitalRead(DOOR_BELL));
+    digitalWrite(LED,IsRingin());
     //delay(10);
     // put your main code here, to run repeatedly:
+}
+
+
+bool IsRingin(){
+    int time = millis();
+    int a = 0;
+    while (millis() - time < 20)
+    {
+        if (digitalRead(DOOR_BELL))
+            a+=3;
+        else
+            a-=1;
+        delay(1);
+    }
+    //Serial.println(a);// 20 wenns klingelt, 60 wenns nicht klingelt
+    return a < 40;
 }
