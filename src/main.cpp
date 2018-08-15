d2c214d5b178a108c4bcf2e30a0d6a5d0a934033
Max Reble <maxreble@googlemail.com>
2018-08-15 15:16:05 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
+
+#define LED 13
+#define DOOR_BELL 23
+
 void setup() {
+    pinMode(LED,OUTPUT);
+    pinMode(DOOR_BELL,INPUT);
+    Serial.begin(9600);
+
     // put your setup code here, to run once:
 }
 
 void loop() {
+   // int read = 
+    //Serial.println(read);
+    digitalWrite(LED,digitalRead(DOOR_BELL));
+    //delay(10);
     // put your main code here, to run repeatedly:
 }
